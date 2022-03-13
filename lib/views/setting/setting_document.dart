b6c4fe4cc4eab9130f72898101be8b5d19d1f968
Jeroen Schweitzer <j.schweitzer@churned.nl>
2022-03-13 15:22:52 +0100

    redid the settings page frame

--- lib/views/setting/setting_document.dart
+++ lib/views/setting/setting_document.dart
@@ -1,10 +1,14 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:fframe/models/setting.dart';
-import 'package:fframe/views/setting/setting.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fframe/constants/fonts.dart';
 
-class settingDocument extends StatelessWidget {
-  const settingDocument({
+import 'package:fframe/views/setting/setting.dart';
+
+class SettingDocument extends StatelessWidget {
+  const SettingDocument({
     required this.setting,
     required this.documentReference,
     Key? key,
@@ -16,24 +20,94 @@ class settingDocument extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Row(
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            HeaderCanvas(context, setting.name!),
-            Divider(),
-            ContentCanvas(context, setting),
-          ],
+        Flexible(
+          child: MainCanvas(context, setting),
+        ),
+        SizedBox(
+          width: 250,
+          child: ContextCanvas(context, setting),
         ),
       ],
     );
   }
 }
 
-class ContentCanvas extends StatelessWidget {
-  const ContentCanvas(BuildContext context, this.setting);
+class MainCanvas extends StatelessWidget {
+  const MainCanvas(BuildContext context, this.setting);
+  final Setting setting;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 1,
+      child: Scaffold(
+        primary: false,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              new SliverAppBar(
+                primary: false,
+                title: DocumentTitle(context, setting.name!),
+                floating: true,
+                pinned: false,
+                snap: true,
+                automaticallyImplyLeading: false,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                // TODO: figure out how to het the appbar here without the tabs
+                bottom: new TabBar(
+                  tabs: <Tab>[
+                    Tab(
+                      icon: Icon(
+                        Icons.directions_car,
+                      ),
+                    ),
+                  ], // <-- total of 2 tabs
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: [
+              SettingsFormSelector(context, setting),
+            ],
+          ),
+        ),
+        floatingActionButton: ExpandableFab(
+          distance: 112.0,
+          children: [
+            ActionButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.close,
+                color: Theme.of(context).floatingActionButtonTheme.foregroundColor,
+              ),
+            ),
+            ActionButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.save,
+                color: Theme.of(context).floatingActionButtonTheme.foregroundColor,
+              ),
+            ),
+            ActionButton(
+              onPressed: () => {},
+              icon: Icon(
+                Icons.add,
+                color: Theme.of(context).floatingActionButtonTheme.foregroundColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsFormSelector extends StatelessWidget {
+  const SettingsFormSelector(BuildContext context, this.setting);
   final Setting setting;
 
   @override
@@ -41,29 +115,30 @@ class ContentCanvas extends StatelessWidget {
     switch (setting.id) {
       case "01-generalsettings":
         {
-          return Row(children: [SettingsGeneralForm()]);
+          return SettingsGeneralForm();
         }
 
       case "02-managelists":
         {
-          return Row(children: [SettingsListsForm()]);
+          return SettingsListsForm();
         }
 
       case "99-advancedsettings":
         {
-          return Row(children: [SettingsAdvancedForm()]);
+          return SettingsAdvancedForm();
         }
 
       default:
         {
-          return Row(children: [Text("unconfigured")]);
+          return Text("unconfigured");
         }
     }
   }
 }
 
 class ContextCanvas extends StatelessWidget {
-  const ContextCanvas(BuildContext context);
+  const ContextCanvas(BuildContext context, this.setting);
+  final Setting setting;
 
   @override
   Widget build(BuildContext context) {
@@ -73,12 +148,12 @@ class ContextCanvas extends StatelessWidget {
         child: SizedBox(
           width: 250,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(4.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text('context widget'),
-                // Expanded(child: IgnorePointer()),
+                ContextWidgetCard(context, "sidewidget 1"),
+                ContextWidgetCard(context, "sidewidget 2"),
               ],
             ),
           ),
@@ -121,20 +196,287 @@ class HeaderControlWidget extends StatelessWidget {
 }
 
 class DocumentTitle extends StatelessWidget {
-  const DocumentTitle(BuildContext context, this.title);
-  final String title;
+  const DocumentTitle(BuildContext context, this.name);
+  final String name;
 
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.topRight,
+      alignment: Alignment.center,
       child: Container(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
-            "${title.toUpperCase()}  ",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            "${name.toUpperCase()}",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class ExpandableFab extends StatefulWidget {
+  const ExpandableFab({
+    Key? key,
+    this.initialOpen,
+    required this.distance,
+    required this.children,
+  }) : super(key: key);
+
+  final bool? initialOpen;
+  final double distance;
+  final List<Widget> children;
+
+  @override
+  _ExpandableFabState createState() => _ExpandableFabState();
+}
+
+class _ExpandableFabState extends State<ExpandableFab> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _expandAnimation;
+  bool _open = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _open = widget.initialOpen ?? false;
+    _controller = AnimationController(
+      value: _open ? 1.0 : 0.0,
+      duration: const Duration(milliseconds: 250),
+      vsync: this,
+    );
+    _expandAnimation = CurvedAnimation(
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.easeOutQuad,
+      parent: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggle() {
+    setState(() {
+      _open = !_open;
+      if (_open) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        clipBehavior: Clip.none,
+        children: [
+          _buildTapToCloseFab(),
+          ..._buildExpandingActionButtons(),
+          _buildTapToOpenFab(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTapToCloseFab() {
+    return SizedBox(
+      width: 56.0,
+      height: 56.0,
+      child: Center(
+        child: Material(
+          shape: const CircleBorder(),
+          clipBehavior: Clip.antiAlias,
+          elevation: 4.0,
+          child: InkWell(
+            onTap: _toggle,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.close,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildExpandingActionButtons() {
+    final children = <Widget>[];
+    final count = widget.children.length;
+    final step = 90.0 / (count - 1);
+    for (var i = 0, angleInDegrees = 0.0; i < count; i++, angleInDegrees += step) {
+      children.add(
+        _ExpandingActionButton(
+          directionInDegrees: angleInDegrees,
+          maxDistance: widget.distance,
+          progress: _expandAnimation,
+          child: widget.children[i],
+        ),
+      );
+    }
+    return children;
+  }
+
+  Widget _buildTapToOpenFab() {
+    return IgnorePointer(
+      ignoring: _open,
+      child: AnimatedContainer(
+        transformAlignment: Alignment.center,
+        transform: Matrix4.diagonal3Values(
+          _open ? 0.7 : 1.0,
+          _open ? 0.7 : 1.0,
+          1.0,
+        ),
+        duration: const Duration(milliseconds: 250),
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+        child: AnimatedOpacity(
+          opacity: _open ? 0.0 : 1.0,
+          curve: const Interval(0.25, 1.0, curve: Curves.easeInOut),
+          duration: const Duration(milliseconds: 250),
+          child: FloatingActionButton(
+            onPressed: _toggle,
+            child: const Icon(Icons.coffee_maker_outlined),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class _ExpandingActionButton extends StatelessWidget {
+  const _ExpandingActionButton({
+    Key? key,
+    required this.directionInDegrees,
+    required this.maxDistance,
+    required this.progress,
+    required this.child,
+  }) : super(key: key);
+
+  final double directionInDegrees;
+  final double maxDistance;
+  final Animation<double> progress;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: progress,
+      builder: (context, child) {
+        final offset = Offset.fromDirection(
+          directionInDegrees * (math.pi / 180.0),
+          progress.value * maxDistance,
+        );
+        return Positioned(
+          right: 4.0 + offset.dx,
+          bottom: 4.0 + offset.dy,
+          child: Transform.rotate(
+            angle: (1.0 - progress.value) * math.pi / 2,
+            child: child!,
+          ),
+        );
+      },
+      child: FadeTransition(
+        opacity: progress,
+        child: child,
+      ),
+    );
+  }
+}
+
+@immutable
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    Key? key,
+    this.onPressed,
+    required this.icon,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Material(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: theme.colorScheme.primaryContainer,
+      elevation: 4.0,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: theme.colorScheme.primaryContainer,
+      ),
+    );
+  }
+}
+
+@immutable
+class FakeItem extends StatelessWidget {
+  const FakeItem({
+    Key? key,
+    required this.isBig,
+  }) : super(key: key);
+
+  final bool isBig;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+      height: isBig ? 128.0 : 36.0,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+        color: Colors.grey.shade300,
+      ),
+    );
+  }
+}
+
+class ContextWidgetCard extends StatelessWidget {
+  const ContextWidgetCard(BuildContext context, this.title);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).colorScheme.primaryContainer,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+          child: Column(children: [
+            Text(title,
+                style: TextStyle(
+                  fontFamily: mainFontFamily,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.onPrimaryContainer,
+                )),
+            Divider(color: Theme.of(context).colorScheme.onBackground),
+            Text(
+              "injected widget goes here",
+              style: TextStyle(
+                fontSize: 14,
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+            ),
+          ]),
         ),
       ),
     );
