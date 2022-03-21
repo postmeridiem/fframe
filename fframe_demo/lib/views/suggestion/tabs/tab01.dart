2e5d9f8a441e229bad7978ed01e91e56b72413a2
Jeroen Schweitzer <j.schweitzer@churned.nl>
2022-03-21 11:02:25 +0100

    added fields and validators. no save or validator trigger yet

--- fframe_demo/lib/views/suggestion/tabs/tab01.dart
+++ fframe_demo/lib/views/suggestion/tabs/tab01.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fframe/helpers/validator.dart';
+
 import 'package:fframe_demo/models/suggestion.dart';
+import 'package:fframe_demo/services/suggestion_service.dart';
 
 class Tab01 extends StatelessWidget {
   const Tab01({Key? key, required this.suggestion}) : super(key: key);
@@ -14,20 +16,34 @@ class Tab01 extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Author: ${suggestion.createdBy ?? "unknown"}"),
         Expanded(
           child: ListView(
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  readOnly: true,
                   decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Author",
+                  ),
+                  controller: TextEditingController.fromValue(TextEditingValue(text: (suggestion.createdBy ?? "unknown"))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    // hoverColor: Color(0xFFFF00C8),
+                    // hoverColor: Theme.of(context).indicatorColor,
                     border: OutlineInputBorder(),
                     labelText: "Name",
                   ),
                   controller: TextEditingController.fromValue(TextEditingValue(text: suggestion.name!)),
                   validator: (curValue) {
+                    // TODO @AZ: should this be moved into a validator class function entirely to avoid repeating patterns?
                     if (validator.validString(curValue)) {
+                      suggestion.name = curValue;
                       return null;
                     } else {
                       return 'Enter a valid name';
@@ -44,8 +60,9 @@ class Tab01 extends StatelessWidget {
                   ),
                   controller: TextEditingController.fromValue(TextEditingValue(text: suggestion.icon!)),
                   validator: (curValue) {
-                    // TODO @AZ: can this be moved into a validator class function entirely to avoid repeating patterns?
                     if (validator.validIcon(curValue)) {
+                      // apply value to the model
+                      suggestion.icon = curValue;
                       return null;
                     } else {
                       return 'Enter a valid icon string';
