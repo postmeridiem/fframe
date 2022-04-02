dee828550da83e262d73b2cb35aaf37ab52cea28
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-04-02 11:58:13 +0200

    removed self export

--- fframe_demo/lib/views/suggestion/suggestion.dart
+++ fframe_demo/lib/views/suggestion/suggestion.dart
@@ -2,5 +2,4 @@ export 'suggestion_list.dart';
 export 'suggestion_navigation.dart';
 export 'suggestion_screen.dart';
 export 'context_card.dart';
-export 'suggestion.dart';
 export 'tabs/tabs.dart';
