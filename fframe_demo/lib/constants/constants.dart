ae0dd702602aad3edafd46cddfd4644e03bcc958
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-04-11 13:52:23 +0200

    removed some old claims

--- fframe_demo/lib/constants/constants.dart
+++ fframe_demo/lib/constants/constants.dart
@@ -2,16 +2,4 @@ Map<String, String> allClaims = {
   "userViewer": "Allow viewing of all registered users",
   "userEditor": "Allow editing of all registered users",
   // "userCreator": "Creation of a registered user",
-
-  "clientViewer": "Allow viewing of all registered client",
-  "clientEditor": "Allow editing of all registered client",
-  "clientCreator": "Allow creating of a registered client",
-
-  "runConfigViewer": "Allow viewing of all registered runConfigs",
-  "runConfigEditor": "Allow Editing of all registered runConfigs",
-  "runConfigCreator": "Allow creating of a registered runConfig",
-
-  "runViewer": "Allow viewing of all registered run",
-  "runEditor": "Allow Editing of all registered run",
-  "runCreator": "Allow creating of a registered run",
 };
