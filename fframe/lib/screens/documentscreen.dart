8c13a2edb4cb1a26c42e9b419827e63e61098f6f
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-05-03 22:31:06 +0200

    added doclist search field and filter chips

--- fframe/lib/screens/documentscreen.dart
+++ fframe/lib/screens/documentscreen.dart
@@ -42,6 +42,17 @@ class DocumentScreen<T> extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState<T> extends ConsumerState<DocumentScreen<T>> {
+  // any text input originating from the query field ends up here
+  // should somehow get provided to the fframe user in the screen.dart
+  // so they can use it in the query builder. Will try applying a double where()
+  // to apply before it even gets there.
+  late Map<String, Map> doclistqueries = {
+    "q": {"query": "", "selected": true, "removable": false},
+    // "active": {"query": "true", "selected": false, "removable": false},
+    // "email": {"query": "me@jsc.im", "selected": false, "removable": true},
+    // "name": {"query": "Jeroen Schweitzer", "selected": true, "removable": true},
+  };
+
   @override
   void initState() {
     super.initState();
@@ -240,8 +251,47 @@ class _DocumentScreenState<T> extends ConsumerState<DocumentScreen<T>> {
                     padding: const EdgeInsets.only(bottom: 4.0),
                     child: Container(
                       color: Theme.of(context).colorScheme.secondary,
-                      child: const TextField(
-                        autofocus: true,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          queryChips(),
+                          TextField(
+                            autofocus: true,
+                            onChanged: (String query) {
+                              if (query?.substring((query.length - 1)
+                                      .clamp(0, query.length)) ==
+                                  ":") {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                        "TIP: Hit enter after typing ':' to create a filter chip... "),
+                                  ),
+                                );
+                              }
+                              doclistqueries["q"]!["query"] = query;
+                            },
+                            onSubmitted: (String query) {
+                              if (query.contains(":")) {
+                                doclistqueries["q"]!["query"] = "";
+                                Map<String, dynamic> newChip = {
+                                  "query": "true",
+                                  "selected": false,
+                                  "removable": true,
+                                };
+
+                                List queryList = query.split(":");
+                                String chipKey = queryList[0];
+                                newChip["query"] = queryList[1];
+                                doclistqueries[chipKey] = newChip;
+                                // if user types a : here, it should auto chip
+                                // put some trottling in: only one chip at a time
+                                // so we don't overload the firestore index?
+
+                              }
+                              debugPrint("$doclistqueries");
+                            },
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -269,6 +319,55 @@ class _DocumentScreenState<T> extends ConsumerState<DocumentScreen<T>> {
           ),
         ],
       ),
+    );
+  }
+
+  queryChips() {
+    List<Widget> chips = [];
+
+    doclistqueries.forEach(
+      (key, value) {
+        if (key != "q") {
+          debugPrint("Chip created for $key: ${value["query"]}");
+          chips.add(
+            Padding(
+              padding: const EdgeInsets.only(top: 2, bottom: 1),
+              child: InputChip(
+                label: RichText(
+                  text: TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "$key: ",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Theme.of(context).colorScheme.onBackground),
+                      ),
+                      TextSpan(
+                        text: value["query"],
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ],
+                  ),
+                ),
+                backgroundColor: Theme.of(context).colorScheme.background,
+                selected: doclistqueries[key]!["selected"],
+                onSelected: (bool selected) {
+                  // TODO: figure out why this does not work.
+                  doclistqueries[key]!["selected"] = selected;
+                  debugPrint("$doclistqueries");
+                },
+                onDeleted: doclistqueries[key]!["removable"] ? () {} : null,
+              ),
+            ),
+          );
+        }
+      },
+    );
+    return Padding(
+      padding: const EdgeInsets.only(top: 4, left: 4, right: 4, bottom: 0),
+      child: Wrap(children: chips),
     );
   }
 }
