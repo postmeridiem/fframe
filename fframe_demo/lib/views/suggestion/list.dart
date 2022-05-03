2b5c072be515f10e7666c0855f53c44643844909
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-05-03 19:48:32 +0200

    cleaned up selected state colors

--- fframe_demo/lib/views/suggestion/list.dart
+++ fframe_demo/lib/views/suggestion/list.dart
@@ -16,10 +16,9 @@ class SuggestionListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       selected: selected,
-      title: Text(suggestion.name!,
-          style: TextStyle(
-              decoration:
-                  selected ? TextDecoration.underline : TextDecoration.none)),
+      title: Text(suggestion.name!),
+      selectedColor: Theme.of(context).colorScheme.onTertiary,
+      selectedTileColor: Theme.of(context).colorScheme.tertiary,
       leading: Icon(iconMap[suggestion.icon]),
       trailing: Icon(iconMap[suggestion.icon]),
     );
