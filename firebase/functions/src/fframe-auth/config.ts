633a3b9bf9588898b013011d8158442b8dc9004c
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-05-04 17:18:03 +0200

    corrected domain

--- firebase/functions/src/fframe-auth/config.ts
+++ firebase/functions/src/fframe-auth/config.ts
@@ -3,5 +3,5 @@
 export default {
     "initialUserRoles": { roles: ["SuperAdmin", "User"] },
     "defaultUserRoles": { roles: ["User"] },
-    "authorizedEmailMasks": ["@imagin.studio", "@churned.io", "@zwaag.net", "@postmeridiem.net", "dehlak@gmail.com"],
+    "authorizedEmailMasks": ["@imagin.studio", "@churned.nl", "@zwaag.net", "@postmeridiem.net", "dehlak@gmail.com"],
 };
