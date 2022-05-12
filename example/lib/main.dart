c232c03e2a55383dfb2f1e7424c97f21c46c4fad
Jeroen Schweitzer <jeroen.schweitzer@postmeridiem.net>
2022-05-12 15:35:36 +0200

    added render debug code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 import 'package:fframe/fframe.dart';
-
 import 'package:example/firebase_config.dart';
+
 import 'package:example/themes/themes.dart';
 import 'package:example/l10n/l10n.dart';
 
@@ -12,6 +13,16 @@ import 'package:example/views/setting/setting.dart';
 import 'package:example/views/user/user.dart';
 
 void main() {
+  // debugPaintSizeEnabled = false;
+  // debugPaintBaselinesEnabled = false;
+  // debugPaintLayerBordersEnabled = false;
+  // debugPaintPointersEnabled = false;
+  // debugRepaintRainbowEnabled = false;
+  // debugRepaintTextRainbowEnabled = false;
+  // debugDisableClipLayers = false;
+  // debugDisablePhysicalShapeLayers = false;
+  // debugDisableOpacityLayers = false;
+
   runApp(const MainApp());
 }
 
