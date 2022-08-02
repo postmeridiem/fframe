7c3943b1575b0f6fbb3bbc49700806f035df2d50
Arno Zwaag <arno@imagin.studio>
2022-08-02 18:44:08 +0200

    exposed firebase storage

--- fframe/lib/fframe.dart
+++ fframe/lib/fframe.dart
@@ -3,6 +3,7 @@ library fframe;
 export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_auth/firebase_auth.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:flutterfire_ui/firestore.dart';
 export 'package:flutter_riverpod/flutter_riverpod.dart';
 
