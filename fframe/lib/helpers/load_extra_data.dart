3278486d01f4b858fac951b606efff9c15120a55
Arno Zwaag <arno@imagin.studio>
2022-08-03 07:54:35 +0200

    Update to helpers

--- fframe/lib/helpers/load_extra_data.dart
+++ fframe/lib/helpers/load_extra_data.dart
@@ -263,9 +263,9 @@ class FirestoreDocument<T> {
 
   T get data => queryDocument.data();
 
-  save() async {
+  Future<SaveState> save() async {
     debugPrint("Save document ${queryDocument.reference.parent.path} :: ${queryDocument.id}");
-    DatabaseService<T>().updateDocument(collection: queryDocument.reference.parent.path, documentId: queryDocument.id, data: data, fromFirestore: fromFirestore, toFirestore: toFirestore);
+    return DatabaseService<T>().updateDocument(collection: queryDocument.reference.parent.path, documentId: queryDocument.id, data: data, fromFirestore: fromFirestore, toFirestore: toFirestore);
     // return queryDocument.reference.set(queryDocument.data(), SetOptions(merge: true));
   }
 }
