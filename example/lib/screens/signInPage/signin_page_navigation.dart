8714cdd2da092d8f05854b9c02c4815a06fb49a5
Jeroen Schweitzer <j.schweitzer@churned.nl>
2022-08-10 10:41:16 +0200

    flutterfire ui seems fixed. reenabling for testing

--- example/lib/screens/signInPage/signin_page_navigation.dart
+++ example/lib/screens/signInPage/signin_page_navigation.dart
@@ -4,7 +4,7 @@ import 'package:example/screens/signInPage/signin_page_screen.dart';
 final signInPageNavigationTarget = NavigationTarget(
   contentPane: const SignInPage(
     title: 'Welcome to FlutFrame Demo',
-    useFlutterFireUI: false,
+    useFlutterFireUI: true,
   ),
   path: "signin",
   title: "Sign In",
