4b42d6848103877bd2b1fccf93aa1acf6ec3c66d
Jeroen Schweitzer <j.schweitzer@churned.nl>
2022-10-06 19:13:33 +0200

    added Developer role

--- example/lib/constants/constants.dart
+++ example/lib/constants/constants.dart
@@ -4,5 +4,6 @@ Map<String, String> appRoles = {
   "UserEditor": "Allow editing of all registered users",
   "UserAdmin": "Allow all operations on all registered users",
   "RoleManager": "Allow changing roles on users",
+  "Developer": "Allow access to development tools",
   "SuperAdmin": "Super admin",
 };
