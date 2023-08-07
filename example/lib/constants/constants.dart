b33423814f0dbfc9cab89a0be54ce69bdfe778eb
Jeroen Schweitzer <j.schweitzer@churned.nl>
2023-08-07 09:08:05 +0200

    add a role togovern the firestore document access button

--- example/lib/constants/constants.dart
+++ example/lib/constants/constants.dart
@@ -1,9 +1,10 @@
 Map<String, String> appRoles = {
   "User": "Basic access",
+  "SuperAdmin": "Super admin",
+  "FirestoreAccess": "Has access to Firestore records",
   "UserViewer": "Allow viewing of all registered users",
   "UserEditor": "Allow editing of all registered users",
   "UserAdmin": "Allow all operations on all registered users",
   "RoleManager": "Allow changing roles on users",
   "Developer": "Allow access to development tools",
-  "SuperAdmin": "Super admin",
 };
