914e854c34225d7911bd8547738b9fb073b95233
Jeroen Schweitzer <j.schweitzer@churned.nl>
2023-09-18 08:55:07 +0200

    remove unneeded import

--- example/lib/screens/list_grid/screen.dart
+++ example/lib/screens/list_grid/screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:fframe/fframe.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'package:example/models/suggestion.dart';
 import 'list_grid.dart';
