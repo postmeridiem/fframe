c622a28767b5f1ce198c54c939a0a8b1b98f79be
Arno Zwaag <arno@imagin.studio>
2024-02-02 14:52:58 +0100

    solved issue with navrail location

--- example/lib/pages/wait_page.dart
+++ example/lib/pages/wait_page.dart
@@ -9,27 +9,65 @@ class WaitPage extends StatelessWidget {
   Widget build(BuildContext context) {
     String? waitText = Fframe.of(context)?.waitText;
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        CircularProgressIndicator(
-          color: color,
-        ),
-        if (waitText != null)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Text(
-                waitText,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-                textAlign: TextAlign.center,
+    return MaterialApp(
+      theme: Fframe.of(context)!.lightMode,
+      darkTheme: Fframe.of(context)!.darkMode,
+      themeMode: Fframe.of(context)!.themeMode,
+      debugShowCheckedModeBanner: Fframe.of(context)!.debugShowCheckedModeBanner,
+      home: Scaffold(
+        body: (waitText != null)
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 200.0,
+                    child: Stack(
+                      children: <Widget>[
+                        const Center(
+                          child: SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 100),
+                            child: Text(waitText),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
               ),
-            ),
-          ),
-      ],
+      ),
     );
+
+    // return Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [
+    //     CircularProgressIndicator(
+    //       color: color,
+    //     ),
+    //     if (waitText != null)
+    //       Padding(
+    //         padding: const EdgeInsets.all(8.0),
+    //         child: Center(
+    //           child: Text(
+    //             waitText,
+    //             style: TextStyle(
+    //               color: Theme.of(context).colorScheme.onBackground,
+    //             ),
+    //             textAlign: TextAlign.center,
+    //           ),
+    //         ),
+    //       ),
+    //   ],
+    // );
   }
 }
--- fframe/lib/frouter/frouter.dart
+++ fframe/lib/frouter/frouter.dart
@@ -4,7 +4,7 @@ part of '../../fframe.dart';
 @immutable
 // ignore: must_be_immutable
 class FRouterInit extends StatefulWidget {
-  const FRouterInit({
+  FRouterInit({
     super.key,
     required this.navigationConfig,
     this.debugMode = false,
@@ -31,6 +31,53 @@ class _FRouterInitState extends State<FRouterInit> {
   @override
   void initState() {
     super.initState();
+    //Remap parent/child structure
+    NavigationConfig navigationConfig = NavigationConfig.clone(widget.navigationConfig);
+
+    navigationConfig.navigationTargets.forEach(
+      ((NavigationTarget navigationTarget) {
+        //Enforce leading slash
+        if (!navigationTarget.path.startsWith("/")) {
+          navigationTarget.path = "/${navigationTarget.path}";
+        }
+
+        Console.log(
+          "${navigationTarget.title} => ${navigationTarget.path}",
+          scope: "FRouterInit",
+          level: LogLevel.fframe,
+        );
+
+        if (navigationTarget.navigationTabs != null && navigationTarget.navigationTabs!.isNotEmpty) {
+          for (NavigationTab navigationTab in navigationTarget.navigationTabs!) {
+            // ignore: unnecessary_null_comparison
+            if (navigationTab.parentTarget == null) {
+              navigationTab.parentTarget = navigationTarget;
+
+              //Remove leading slash
+              if (navigationTab.path.startsWith("/")) {
+                navigationTab.path = navigationTab.path.substring(1);
+              }
+
+              navigationTab.path = "${navigationTab.parentTarget!.path}/${navigationTab.path}";
+              Console.log(
+                "${navigationTab.parentTarget!.title} => ${navigationTab.path}",
+                scope: "FRouterInit",
+                level: LogLevel.fframe,
+              );
+            } else {
+              Console.log(
+                "Already set: ${navigationTab.parentTarget!.title} => ${navigationTab.path}",
+                scope: "FRouterInit",
+                level: LogLevel.fframe,
+              );
+            }
+          }
+        } else if (!navigationTarget.path.startsWith("/")) {
+          navigationTarget.path = "/${navigationTarget.path}";
+        }
+      }),
+    );
+
     FRouterConfig(
       navigationConfig: widget.navigationConfig,
       routerBuilder: widget.routerBuilder,
@@ -45,7 +92,7 @@ class _FRouterInitState extends State<FRouterInit> {
         builder: (context, ref, _) {
           navigationNotifier = ref.read(navigationProvider);
           routerDelegate = FNavigationRouterDelegate();
-          return widget.routerBuilder(context);
+                    return widget.routerBuilder(context);
         },
       ),
     );
