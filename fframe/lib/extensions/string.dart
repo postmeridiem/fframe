c61bf02b21a49ac14f60c892113f36c277823d8c
Jeroen Schweitzer <jeroen@imagin.studio>
2025-07-16 17:06:33 +0200

    add new string control functions

--- fframe/lib/extensions/string.dart
+++ fframe/lib/extensions/string.dart
@@ -5,5 +5,17 @@ extension StringExtension on String? {
     }
     return this;
   }
+
+  String? toCapitalized() {
+    if (this != null && this!.isNotEmpty) {
+      return "${this![0].toUpperCase()}${this!.substring(1)}";
+    }
+    return this;
+  }
+
+  String? toTitleCase() {
+    if (this == null) return null;
+    return this!.split(' ').map((word) => word.toCapitalized()).join(' ');
+  }
 }
 
